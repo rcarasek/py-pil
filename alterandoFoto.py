0da0727f6727396a82b1ea599927d0704c4f0bee
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2021-02-26 14:28:29 -0300

    B

--- alterandoFoto.py
+++ alterandoFoto.py
@@ -1,9 +1,10 @@
-from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter, ImageOps
-import PIL 
+from PILLOW import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter, ImageOps
+import PILLOW 
 
-iii= PIL.Image.open('PequenoDatilo.jpg')
+iii= PILLOW.Image.open('PequenoDatilo.jpg')
 
 draw = ImageDraw(iii)
 font = ImageFont.truetype('arial.fft', size = 80)
 draw.text((50,50), 'hello world', fill = 'rgb(0,0,0) ', font=font)
 iii.save('PequenoDatilo_Draw.jpg')
+
